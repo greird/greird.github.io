e02e70502a845ed5154765d13378d3e297e9cf2d
greird <h.fauconnier@gmail.com>
2014-01-12 12:07:59 +0100

    mend
    add js

--- js/animate.js
+++ js/animate.js
@@ -7,10 +7,12 @@ $( document ).ready(function() {
 	$("#tags-legend .label-warning").click(
 		function() {
 			if (inprogress == 1) {
+				$(this).stop( true, true ).fadeTo( "fast", 0.50 );
     			$(".tags .label-warning").stop( true, true ).hide();
     			inprogress = 0;
     		}
     		else {
+    			$(this).stop( true, true ).fadeTo( "fast", 1 );
     			$(".tags .label-warning").stop( true, true ).show();
     			inprogress = 1;
     		}
@@ -19,14 +21,13 @@ $( document ).ready(function() {
 	$("#tags-legend .label-primary").click(
 		function() {
 			if (standby == 1) {
-    			$(".tags .label-warning").stop( true, true ).show();
-    			$(".tags .label-success").stop( true, true ).show();
+				$(this).stop( true, true ).fadeTo( "fast", 0.50 );
+    			$(".tags .label-primary").stop( true, true ).hide();
     			standby = 0;
     		}
     		else {
+    			$(this).stop( true, true ).fadeTo( "fast", 1 );
     			$(".tags .label-primary").stop( true, true ).show();
-    			$(".tags .label-warning").stop( true, true ).hide();
-    			$(".tags .label-success").stop( true, true ).hide();
     			standby = 1;
     		}
     		console.log("in progress visible = "+inprogress+"; standby visible = "+standby+"; done visible = "+done);
@@ -34,14 +35,13 @@ $( document ).ready(function() {
 	$("#tags-legend .label-success").click(
 		function() {
 			if (done == 1) {
-    			$(".tags .label-primary").stop( true, true ).show();
-    			$(".tags .label-warning").stop( true, true ).show();
+				$(this).stop( true, true ).fadeTo( "fast", 0.50 );
+    			$(".tags .label-success").stop( true, true ).hide();
     			done = 0;
     		}
     		else {
+    			$(this).stop( true, true ).fadeTo( "fast", 1 );
     			$(".tags .label-success").stop( true, true ).show();
-    			$(".tags .label-primary").stop( true, true ).hide();
-    			$(".tags .label-warning").stop( true, true ).hide();
     			done = 1;
     		}
     		console.log("in progress visible = "+inprogress+"; standby visible = "+standby+"; done visible = "+done);
