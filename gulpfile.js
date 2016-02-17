24dcbfc65a1f11528116594518f911429fac3474
greird <h.fauconnier@gmail.com>
2016-02-17 13:01:43 +0100

    Concatenate css

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,35 @@ var gulp = require('gulp'),
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('js/main.js')
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify({ preserveComments: "license" }))
         .pipe(gulp.dest('js'))
 });
 
+// Concatenate CSS
+gulp.task('css', function() {
+    return gulp.src(['css/normalize.css', 'css/main.css', 'css/mobile.css'])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('css'))
+});
+
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('js/main.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Default task
  gulp.task('default', ['watch'], function() {
-   gulp.start('lint', 'scripts');
+   gulp.start('lint', 'scripts', 'css');
  });
 
 // Watch
 gulp.task('watch', function() {
 
-  // Watch .js files
-  gulp.watch('src/*.js', ['lint', 'scripts']);
+  gulp.watch('js/main.js', ['lint', 'scripts']);
+  gulp.watch(['css/normalize.css', 'css/main.css', 'css/mobile.css'], ['css']);
 
 });
