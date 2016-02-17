294062cb754b273354a481da01a2c364a1b4bca3
greird <h.fauconnier@gmail.com>
2016-02-17 12:40:41 +0100

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Load plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat');
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify({ preserveComments: "license" }))
+        .pipe(gulp.dest('js'))
+});
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Default task
+ gulp.task('default', ['watch'], function() {
+   gulp.start('lint', 'scripts');
+ });
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .js files
+  gulp.watch('src/*.js', ['lint', 'scripts']);
+
+});
