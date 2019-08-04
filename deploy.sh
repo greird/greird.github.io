1a613fadc5e4f37dc1a2ac4082121ddac60b172b
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-04 11:39:11 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ set -e
 
 gulp build
 
+git add -A
+git commit -m 'deploy'
+git push https://github.com/greird/greird.github.io.git master:develop
+
 cd dist
 
 echo 'www.hubertfauconnier.com' > CNAME
