1faddbe84b097bc6cc42e902fd142ccd046753fe
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-04 11:46:40 +0200

    fixing file structure

--- gulpfile.js
+++ gulpfile.js
@@ -13,50 +13,38 @@ const browserSync = require('browser-sync').create();
 
 // Erase build directory
 function cleandist() {
-	return gulp.src('dist', {read: false, allowEmpty: true})
+	return gulp.src(['css', 'js'], {read: false, allowEmpty: true})
 	.pipe(clean())
 }
 
 // Concatenate & Minify JS
 function minify() {
-	return gulp.src('js/main.js')
+	return gulp.src('src/js/main.js')
 	.pipe(rename({ suffix: '.min' }))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'))
+	.pipe(gulp.dest('js'))
 }
 
 // Lint Task
 function lint() {
-	return gulp.src('js/main.js')
+	return gulp.src('src/js/main.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'))
 }
 
 // Concatenate CSS
 function concatcss() {  
-	return gulp.src(['css/normalize.css', 'css/main.css', 'css/mobile.css'])
+	return gulp.src(['src/css/normalize.css', 'src/css/main.css', 'src/css/mobile.css'])
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(concatCss('main.css'))
-	.pipe(gulp.dest('dist/css'))
-}
-
-// Move HTML to dist 
-function moveHTMLToDist() {
-	return gulp.src(['index.html'])
-	.pipe(gulp.dest('dist'))
-}
-
-// Move img to dists
-function moveImgToDist() {
-	return gulp.src(['img/*.*'])
-	.pipe(gulp.dest('dist/img'))
+	.pipe(gulp.dest('css'))
 }
 
 function serve() {
 	// Static server
 	browserSync.init({
 		server: {
-			baseDir: "./dist/"
+			baseDir: "./"
 		}
 	});
 
@@ -66,5 +54,5 @@ function serve() {
 
 exports.lint = lint;
 
-exports.build = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), gulp.parallel(moveHTMLToDist, moveImgToDist));
-exports.default = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), gulp.parallel(moveHTMLToDist, moveImgToDist), serve);
+exports.build = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)));
+exports.default = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), serve);
