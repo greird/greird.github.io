ddb2f28c829f9d997796f025df3128eb7a3b4d4c
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-04 11:27:14 +0200

    revamped file structure; will deploy site in /dist and to another branch

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+gulp
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'www.hubertfauconnier.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/greird/greird.github.io.git master:gh-pages
+
+cd -
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 
 const { src, dest, series, parallel } = require('gulp');
+const clean = require('gulp-clean');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const jshint = require('gulp-jshint');
@@ -10,34 +11,52 @@ const concatCss = require('gulp-concat-css');
 const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
 
+// Erase build directory
+function cleandist() {
+	return gulp.src('dist', {read: false, allowEmpty: true})
+	.pipe(clean())
+}
+
 // Concatenate & Minify JS
 function minify() {
-	return gulp.src('src/js/main.js')
+	return gulp.src('js/main.js')
 	.pipe(rename({ suffix: '.min' }))
 	.pipe(uglify())
-	.pipe(gulp.dest('js'))
+	.pipe(gulp.dest('dist/js'))
 }
 
 // Lint Task
 function lint() {
-	return gulp.src('src/js/main.js')
+	return gulp.src('js/main.js')
 	.pipe(jshint())
-	.pipe(jshint.reporter('default'));
+	.pipe(jshint.reporter('default'))
 }
 
 // Concatenate CSS
 function concatcss() {  
-	return gulp.src(['src/css/normalize.css', 'src/css/main.css', 'src/css/mobile.css'])
+	return gulp.src(['css/normalize.css', 'css/main.css', 'css/mobile.css'])
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(concatCss('main.css'))
-	.pipe(gulp.dest('css'))
+	.pipe(gulp.dest('dist/css'))
+}
+
+// Move HTML to dist 
+function moveHTMLToDist() {
+	return gulp.src(['index.html'])
+	.pipe(gulp.dest('dist'))
+}
+
+// Move img to dists
+function moveImgToDist() {
+	return gulp.src(['img/*.*'])
+	.pipe(gulp.dest('dist/img'))
 }
 
 function serve() {
 	// Static server
 	browserSync.init({
 		server: {
-			baseDir: "./"
+			baseDir: "./dist/"
 		}
 	});
 
@@ -45,9 +64,7 @@ function serve() {
 	gulp.watch("index.html").on('change', browserSync.reload);
 }
 
-exports.minify = minify;
 exports.lint = lint;
-exports.concatcss = concatcss;
-exports.serve = serve;
 
-exports.default = gulp.series(gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), serve);
+exports.build = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), gulp.parallel(moveHTMLToDist, moveImgToDist));
+exports.default = gulp.series(cleandist, gulp.parallel(gulp.series(lint, minify), gulp.series(concatcss)), gulp.parallel(moveHTMLToDist, moveImgToDist), serve);
