f56a4f7162a7e318f812615f9494a61a1b029767
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-04 11:32:50 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,16 @@
 # abort on errors
 set -e
 
-# build
 gulp build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
 echo 'www.hubertfauconnier.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/greird/greird.github.io.git master:gh-pages
+git push -f https://github.com/greird/greird.github.io.git master
 
 cd -
